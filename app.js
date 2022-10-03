94b945ddbccf9fce9b9087b785d24864c3d253cb
GovindaTakekar <72185898+GovindaTakekar@users.noreply.github.com>
2022-10-03 12:13:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 import { Express } from "express";
 const app = Express();
 
-app.listen(3000,'server started bro');
+PORT = 3000;
+
+app.get("/" , (req , res) => {
+   res.send("server pages are also visible bro");
+});
+
+app.listen(PORT,`server started bro on ${PORT} `);
